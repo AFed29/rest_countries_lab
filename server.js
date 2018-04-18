275785c5ab0771970bab402af47d5f84d8c998e9
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2018-04-18 11:17:37 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(`${ __dirname }/index.html`);
+});
+
+app.listen(3000, function () {
+  console.log(`Alex's and Kishan's app listening on port ${this.address()
