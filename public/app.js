98bc759255fdb6c92d377997a412fa57e50b1271
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2018-04-18 14:14:01 +0100

    Added ability to see bordering country details

--- public/app.js
+++ public/app.js
@@ -29,6 +29,7 @@ const populateList = function (countries) {
   countries.forEach((country) => {
     const option = document.createElement('option');
     option.textContent = country.name;
+    option.value = country.alpha3Code;
     select.appendChild(option);
   });
 }
@@ -36,28 +37,60 @@ const populateList = function (countries) {
 const handleCountryChange = function (event) {
   const country = getCountryData(this.value);
   populateCountryData(country);
+  populateBorderCountries(country);
 }
 
-const getCountryData = function (countryName) {
+const getCountryData = function (alpha3Code) {
     return countriesData.find(function (country){
-    return country.name === countryName;
+    return country.alpha3Code === alpha3Code;
     });
 }
 
 const populateCountryData = function (country) {
   const countryDataList = document.querySelector('#selected-country-data');
   countryDataList.innerHTML = ''
-  const countryData = [];
+  const countryData = {};
 
-  countryData.push(country.name);
-  countryData.push(country.population);
-  countryData.push(country.capital);
+  countryData['Country Name'] = country.name;
+  countryData['Population'] = country.population;
+  countryData['Capital City'] = country.capital;
 
-  console.log(countryData);
-
-  for (const data of countryData) {
+  for (const key in countryData) {
     const li = document.createElement('li');
-    li.textContent = data;
+    li.textContent = `${ key }: ${countryData[key]}`;
     countryDataList.appendChild(li);
+  }
+}
+
+const populateBorderCountries = function (country) {
+  const borderCountriesDiv = document.querySelector('#border-countries')
+  borderCountriesDiv.innerHTML = '';
+  if (country.borders.length === 0) {
+    const message = document.createElement('p')
+    message.textContent = `No countries border ${ country.name }`;
+    borderCountriesDiv.appendChild(message);
+  } else {
+    const h3 = document.createElement('h3');
+    h3.textContent = `Countries Bordering ${ country.name }:`
+    borderCountriesDiv.appendChild(h3);
+
+    for (countryCode of country.borders) {
+
+      const ul = document.createElement('ul');
+      borderCountriesDiv.appendChild(ul);
+      const countryData = {};
+
+      const borderCountry = getCountryData(countryCode);
+
+      countryData['Country Name'] = borderCountry.name;
+      countryData['Population'] = borderCountry.population;
+      countryData['Capital City'] = borderCountry.capital;
+
+      for (const key in countryData) {
+        const li = document.createElement('li');
+        li.textContent = `${ key }: ${countryData[key]}`;
+        ul.appendChild(li);
+      }
+    }
   }
 }
